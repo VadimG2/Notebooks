b0191945318f8c95c8aef80efa57843b148a95b5
VadimM <vad.gvozdew@yandex.ru>
2024-05-20 19:46:30 +0300

    Create notebooks set

--- MainNotebookSshop.java
+++ MainNotebookSshop.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainNotebookSshop {
     public static void main(String[] args) {
         // Создаем ноутбуки
@@ -7,5 +11,14 @@ public class MainNotebookSshop {
         NotebookSshop notebook4 = new NotebookSshop(4, 32, 3072, "Windows", "Black");
         NotebookSshop notebook5 = new NotebookSshop(5, 16, 1024, "MacOS", "White");
         NotebookSshop notebook6 = new NotebookSshop(6, 8, 512, "Windows", "White");
+    
+            // Создаем множество ноутбуков и добавляем в него созданные ноутбуки
+        Set<NotebookSshop> notebooks = new HashSet<>(
+                Arrays.asList(notebook1, notebook2, notebook3, notebook4, notebook5, notebook6));
+            // Выводим информацию о всех ноутбуках
+        System.out.println("Все ноутбуки:");
+        for (NotebookSshop notebook : notebooks) {
+            System.out.println(notebook);
+        }
     }
 }
