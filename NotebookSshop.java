6e5c58d5f12fee072b8ac520accd4729fee18e4a
VadimM <vad.gvozdew@yandex.ru>
2024-05-20 19:38:46 +0300

    Add set's and get's

--- NotebookSshop.java
+++ NotebookSshop.java
@@ -1,17 +1,55 @@
 public class NotebookSshop {
-    public class NotebookShop {
-        private int id;
-        private int RAM;
-        private int HDDSize;
-        private String OS;
-        private String color;
-    
-        public NotebookShop(int id, int RAM, int HDDSize, String OS, String color) {
-            this.id = id;
-            this.RAM = RAM;
-            this.HDDSize = HDDSize;
-            this.OS = OS;
-            this.color = color;
-        }
+    private int id;
+    private int RAM;
+    private int HDDSize;
+    private String OS;
+    private String color;
+
+    public NotebookSshop(int id, int RAM, int HDDSize, String OS, String color) {
+        this.id = id;
+        this.RAM = RAM;
+        this.HDDSize = HDDSize;
+        this.OS = OS;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public int getHDDSize() {
+        return HDDSize;
+    }
+
+    public String getOS() {
+        return OS;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRAM(int RAM) {
+        this.RAM = RAM;
+    }
+
+    public void setHDDSize(int HDDSize) {
+        this.HDDSize = HDDSize;
+    }
+
+    public void setOS(String OS) {
+        this.OS = OS;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
