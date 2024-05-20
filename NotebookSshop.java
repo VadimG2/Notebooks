6d5377201345dc0ad57348e16b67ff246cc32b41
VadimM <vad.gvozdew@yandex.ru>
2024-05-20 19:34:39 +0300

    Add variables and constructor

--- NotebookSshop.java
+++ NotebookSshop.java
@@ -1,3 +1,17 @@
 public class NotebookSshop {
+    public class NotebookShop {
+        private int id;
+        private int RAM;
+        private int HDDSize;
+        private String OS;
+        private String color;
     
+        public NotebookShop(int id, int RAM, int HDDSize, String OS, String color) {
+            this.id = id;
+            this.RAM = RAM;
+            this.HDDSize = HDDSize;
+            this.OS = OS;
+            this.color = color;
+        }
+    }
 }
