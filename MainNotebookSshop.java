0186dca1ca5cbcd3b062006289a485640ef47a69
VadimM <vad.gvozdew@yandex.ru>
2024-05-20 19:50:01 +0300

    Ask criteria from user method

--- MainNotebookSshop.java
+++ MainNotebookSshop.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class MainNotebookSshop {
     public static void main(String[] args) {
@@ -19,6 +22,27 @@ public class MainNotebookSshop {
         System.out.println("Все ноутбуки:");
         for (NotebookSshop notebook : notebooks) {
             System.out.println(notebook);
+        }
+                // Создаем критерии фильтрации
+        Map<Integer, String> criteria = new TreeMap<>();
+        criteria.put(1, "RAM");
+        criteria.put(2, "HDD");
+        criteria.put(3, "OS");
+        criteria.put(4, "Color");
+    }
+        // Получение критерия фильтрации от пользователя
+    public static int getCriterionFromUser(Map<Integer, String> criteria) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите цифру, соответствующую необходимому критерию:");
+        printCriteria(criteria); // Выводим список критериев
+        return scanner.nextInt();
+    }
+
+    // Вывод списка критериев на экран
+    public static void printCriteria(Map<Integer, String> criteria) {
+        for (Map.Entry<Integer, String> entry : criteria.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
 }
